c3f03f47209605d3e2b209a4270dc9bf6184ce59
bryantb148 <90650979+bryantb148@users.noreply.github.com>
2022-02-01 14:39:01 -0700

    yuh

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document .getElementbyId ("btn");
+const color = document.queryselector(".color");
+
+btn.addEventListener("click", function () {
+    const randomNumber = getrandomnumber();   
+    console.log(randomNumber)
+});
+
+function getRandomNumber() {
+    return Math.random
+}                   
+                    
+
+
+
+
+
--- hex.js
+++ hex.js
@@ -1,1 +1,11 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElemnetById("btn");
+const color = document.querySelctor(".color");
+
+btn.addEvebtListner("click", function (){
+    let hexColor = "#";  
+    for(let i = 0; i <6; i++){
+        
+    }
+console.log(hexcolor);    
+})
